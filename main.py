73eefd56b0ff2e9b03787dab302669f14dc7d980
w1ghton <88480665+w1ghton@users.noreply.github.com>
2025-11-24 12:14:49 +0300

    add support for EasyMP4

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import mutagen
+import mutagen.easymp4
 import re
 import os
 
@@ -8,7 +9,7 @@ def search_music(folder_path):
         for file in files:
             file_path = os.path.join(root, file)
             try:
-                if mutagen.File(file_path) is not None:
+                if mutagen.File(file_path, easy=True) is not None:
                     change_artist(file_path)
             except Exception:
                 continue
@@ -19,18 +20,30 @@ def change_artist(file_path: str):
         print(f"Error: {file_path} isn't a file")
         return
 
-    file = mutagen.File(file_path)
+    file = mutagen.File(file_path, easy=True)
     pattern = r"\sft.?\s|\sfeat.?\s|\sfeaturing\s|\s?;\s?|\s?&\s?|\sand\s"
 
-    print(f"{os.path.basename(file_path)}: {file["artist"]} ->", end=" ")
+    try:
+        artists_tag = file.get("artist")
+        if not artists_tag:
+            return
 
-    artists = []
-    for artist in file["artist"]:
-        artists.extend(re.split(pattern, artist))
+        print(f"{os.path.basename(file_path)}: {artists_tag} ->", end=" ")
 
-    print(artists)
-    file["artist"] = artists
-    file.save()
+        artists = []
+        for artist in artists_tag:
+            artists.extend(re.split(pattern, artist))
+
+        print(artists)
+
+        if isinstance(file, mutagen.easymp4.EasyMP4):
+            file["artist"] = ["/".join(artists)]
+        else:
+            file["artist"] = artists
+
+        file.save()
+    except Exception as e:
+        print(f"Error processing {file_path}: {e}")
 
 
 if __name__ == "__main__":
